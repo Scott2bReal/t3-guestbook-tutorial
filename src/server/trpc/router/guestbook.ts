20aacdbd138dec1abaf9d4490d8cdd1eed80f28f
Scott Hoecker <hoeckers@gmail.com>
2022-10-06 14:11:09 -0500

    I think the backend is working

--- src/server/trpc/router/guestbook.ts
+++ src/server/trpc/router/guestbook.ts
@@ -1,4 +1,39 @@
-import { z } from 'zod';
-import { t } from '../trpc'
+import { z } from 'zod'
+import { authedProcedure, t } from '../trpc'
 
-const guestbookRouter
+export const guestbookRouter = t.router({
+  findMany: t.procedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.prisma.guestbook.findMany({
+        select: {
+          name: true,
+          message: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      })
+    } catch (error) {
+      console.log('error', error)
+    }
+  }),
+  postMessage: authedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        message: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.guestbook.create({
+          data: {
+            name: input.name,
+            message: input.message,
+          },
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }),
+})
